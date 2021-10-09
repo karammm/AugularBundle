7a151904fecd09cf9a19faca0c9a20eff8cb0eed
Karamijit <karamjitverma9@gmail.com>
2021-10-10 01:33:32 +0530

    cleaning app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 // import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { FormsModule } from '@angular/forms';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './SuccessAlert/successalert.component';
-import { WarningAlertComponent } from './WarningAlert/warningalet.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent,
-    ServersComponent//Telling the app that this component is a part of this app
   ],
   imports: [
     BrowserModule,
