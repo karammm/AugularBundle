5346cf3adf82aae0e3562e5f89e38a8dfbe22e13
Karamijit <karamjitverma9@gmail.com>
2021-11-07 19:06:36 +0530

    ingredients model

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,5 +1,12 @@
 export class Ingredient{
-    public name:string;
-    public amount: number;
-    
+    //no need to create properties as below
+    //insted provide public inside the constructoe it will create the same effect
+    // public name:string;
+    // public amount: number;
+
+    constructor(public name:string,public amount:number){
+        //NO NEED
+        // this.name=name;
+        // this.amount=amount;
+    }
 }
